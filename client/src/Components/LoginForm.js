cf17829d481bedd92eab70a5f435775c45cd3d50
Anthony Feeney <vigilantenighthawk@gmail.com>
2020-04-02 20:01:36 -0400

    Login form is now able to connect to the server.  Server is able to send welcome message back.

--- client/src/Components/LoginForm.js
+++ client/src/Components/LoginForm.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
+import openSocket from 'socket.io-client';
 
 export default class LoginForm extends Component{
 
     render(){
-        return <form>
+        return <form onSubmit={this.connect}>
             <p>
-                <label forHtml="username">User Name: </label>
+                <label htmlFor="username">User Name: </label>
                 <input type="text" id="username" name="username" />
             </p>
             <p>
                 <button type="submit">Connect</button>
             </p>
         </form>;
+    }
+
+    connect = (event) =>{
+        event.preventDefault();
+        const socket = openSocket("http://localhost:3001");
+        socket.on('welcome',(data) => {
+            console.log(data);
+        })
+
     }
 }
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.use(cors());
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-server.listen(env.process.CHAT_PORT || 3001);
+server.listen(process.env.CHAT_PORT || 3001);
 
 io.on('connection', socket =>{
     socket.emit('welcome',{message:"Welcome to the chat"});
